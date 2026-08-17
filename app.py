219ecd7f741b49ddd5decfa36f8468fab9c9494d
JaydenLal <jl22113@student.phs.school.nz>
2026-08-17 18:09:49 +1200

    added error handler webpage, removed unneccesary elements from index.html

--- app.py
+++ app.py
@@ -27,5 +27,10 @@ def index():
     
     return render_template('index.html', records=records)
 
+# error handler for 404 errors
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
