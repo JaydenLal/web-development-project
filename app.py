2fe893a19a5a5392fe553b6e15e49c4175ccc2df
JaydenLal <jl22113@student.phs.school.nz>
2026-08-18 09:46:33 +1200

    add drop down box

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-# This route handles the homepage and loads data to display on the page
+# This connects the database and loads the database to display on index.html
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -32,6 +32,27 @@ def index():
 
     # Send the data to the template so it can show rows in the HTML table
     return render_template('index.html', records=records)
+
+# This connects the database and loads the database to display on index2.html
+@app.route('/index2')
+def index2():
+    conn = get_db_connection()
+
+    # SQL query: get player names, game, school, year, and score
+    # from the related tables, sorted from lowest score to highest
+    query = '''
+        SELECT s.first_name, g.game, s.school, t.year, tm.score
+        FROM tournament_matches tm
+        JOIN student s ON tm.student_id = s.student_id
+        JOIN game g ON tm.game_id = g.game_id
+        JOIN tournament t ON tm.tournament_id = t.tournament_id
+        ORDER BY tm.score ASC
+    '''
+
+    records = conn.execute(query).fetchall()
+    conn.close()
+
+    return render_template('index2.html', records=records)
 
 # If someone visits a page that does not exist, show the 404 page
 @app.errorhandler(404)
